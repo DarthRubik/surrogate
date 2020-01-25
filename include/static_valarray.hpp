8dc79e98d68c8df895f08cfe90625c81f37ea519
Jacob Gamble <gamblejr@prototypeInd.com>
2020-01-25 11:31:24 -0600

    Adding unary operators and a test case

--- include/static_valarray.hpp
+++ include/static_valarray.hpp
@@ -1,5 +1,35 @@
 #ifndef STATIC_VALARRAY_HPP
 #define STATIC_VALARRAY_HPP
+#include <cstdint>
+#include <utility>
 
+
+template<typename value_t, std::size_t size>
+struct static_valarray
+{
+    // Purposefully an aggregate for aggregate initialization
+    value_t inner[size];
+#define UNARY_OPERATOR(op, constness)\
+    constexpr auto operator op() constness\
+    {\
+        using new_type = decltype(op std::declval<value_t constness>());\
+        static_valarray<new_type,size> ret;\
+        for (std::size_t x = 0; x < size; x++)\
+            ret.inner[x] = op this->inner[x];\
+        return ret;\
+    }
+
+    UNARY_OPERATOR(+,)
+    UNARY_OPERATOR(+,const)
+
+    UNARY_OPERATOR(-,)
+    UNARY_OPERATOR(-,const)
+
+    UNARY_OPERATOR(~,)
+    UNARY_OPERATOR(~,const)
+
+    UNARY_OPERATOR(!,)
+    UNARY_OPERATOR(!,const)
+};
 
 #endif
--- test/static_valarray.cpp
+++ test/static_valarray.cpp
@@ -1,9 +1,39 @@
 #include <cassert>
 #include <static_valarray.hpp>
-
+#include <type_traits>
 
 
 int main()
 {
-	assert(true);
+    {
+        // Test unary plus
+        int array[3] = { 1, 2, 3};
+        using array_t = decltype(array);
+        using test_t = static_valarray<array_t,2>;
+        test_t x = {
+            {
+                {1,2,3},
+                {4,5,6}
+            }
+        };
+
+        //Unary plus operator on arrays decays into a pointer
+        auto thing = +x;
+        // Since this is a pointer the original will change
+        thing.inner[0][0] = 12;
+        assert(x.inner[0][0] == 12);
+        static_assert(
+            std::is_same<
+                static_valarray<int*,2>,
+                decltype(thing)
+            >::value, "");
+
+        auto other_thing = +const_cast<test_t const&>(x);
+        assert(other_thing.inner[0][0] == 12);
+        static_assert(
+            std::is_same<
+                static_valarray<int const*,2>,
+                decltype(other_thing)
+            >::value, "");
+    }
 }
